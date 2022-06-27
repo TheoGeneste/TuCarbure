f048b4a636caae83c3295f9a121d582c259cf965
TheoGeneste <tgeneste@alliance-digital.fr>
2022-06-27 14:44:41 +0200

    Ajout de la page de login, modification de la navbar, ajout de la geoloc

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TuCarbure',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: MainPage(),
     );
