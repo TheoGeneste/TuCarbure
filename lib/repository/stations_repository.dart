fae20bc72622eba53bd3ce1c0eb301717081aa72
TheoGeneste <tgeneste@alliance-digital.fr>
2022-06-28 16:27:37 +0200

    Ajour des points recuperer via l'api sur la map sur un rayon en brut de 100km

--- lib/repository/stations_repository.dart
+++ lib/repository/stations_repository.dart
@@ -4,12 +4,10 @@ import 'package:tu_carbure/data/stations_data.dart';
 import 'package:tu_carbure/model/station.dart';
 
 class StationRepository{
-  Future<void> getStationWithPerimetre(longitude, latitude, perimetre) async {
+  Future<List<Map<String, dynamic>>> getStationWithPerimetre(longitude, latitude, perimetre) async {
     var stations = await StationsData().getStationWithPerimetre(longitude, latitude, perimetre);
-    final parsed = List<Map<String, dynamic>>.from(json.decode(stations.toString()));
-    print(parsed);
-    // (jsonDecode(stations.toString())).map((e){
-    //   print(e);
-    // });
+    final parsed = List<Map<String, dynamic>>.from(json.decode(stations)['stations']);
+
+    return parsed;
   }
 }
--- lib/view/viewmodels/stations_viewmodel.dart
+++ lib/view/viewmodels/stations_viewmodel.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:tu_carbure/repository/stations_repository.dart';
 
 class StationViewModel with ChangeNotifier{
-  void getStationInPerimetre(longitude, latitude, perimetre){
-    var stations = StationRepository().getStationWithPerimetre(longitude, latitude, perimetre);
+  Future<List<Map<String, dynamic>>> getStationInPerimetre(longitude, latitude, perimetre) async {
+    var stations = await StationRepository().getStationWithPerimetre(longitude, latitude, perimetre);
+    return stations;
   }
 }
